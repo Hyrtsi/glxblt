9ed21e69d1b0d4a22b02c0cfbb2d342e748d42fa
Eljas Hyyrynen <eljas.hyyrynen@basemark.com>
2022-01-18 22:01:40 +0200

    draw triangle

--- glfw-test/main.cpp
+++ glfw-test/main.cpp
@@ -35,11 +35,8 @@ int main()
     // without backwards-compatible features we no longer need
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Needed on MAC OSX
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
     GLFWwindow* window = glfwCreateWindow(800, 600, "openGL is fun", NULL, NULL);
-    if (window == NULL)
+    if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -56,24 +53,138 @@ int main()
         return -1;
     }
 
+    // -----------------------------------
+
     // Set the viewport
     glViewport(0, 0, 800, 600);
 
     // Adjust the viewport if user resizes the window
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // -------------------- compile shaders ---
+
+
+    // Create a vertex shader and compile it
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+
+    int  success{0};
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (success == 0)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Create a fragment shader and compile it
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\0";
+    
+    uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (success == 0)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Create a shader program
+    // final linked version of multiple shaders combined
+    uint32_t shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (success == 0)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+
+    // Delete the unnecessary shaders as they are now linked to the shader program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);  
+
+    // ------------- compiled shaders
+    
+    // todo: change
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+    // Reserve the buffer in GPU memory
+    uint32_t vertexBufferObjects{0};
+    uint32_t VAO{0};
+    glGenBuffers(1, &vertexBufferObjects);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjects);
+
+    // Copy the vertices to the array buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);  
+
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO
+    // as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+
+
+    // Activate the shader program
+    glUseProgram(shaderProgram);
+
     // Render loop
     while(!glfwWindowShouldClose(window))
     {
         processInput(window);
 
+
         // TODO: render();
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &vertexBufferObjects);
+    glDeleteProgram(shaderProgram);
+
     printf("Successfully exited the program!\n");
     
     glfwTerminate();
