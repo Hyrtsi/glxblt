b31177671ef63f5fd545ff7ed043fbbd3f4ca843
Eljas Hyyrynen <eljas.hyyrynen@basemark.com>
2022-02-05 16:08:53 +0200

    add matituls

---
+++ glfw-test/matutils.cpp
@@ -0,0 +1,16 @@
+#include "matutils.hpp"
+
+Mat4d perspectiveMatrix(double fovRad, double near, double far)
+{
+    const double S = 1.0 / (tan(fovRad*0.5));
+    const double foo = -far / (far-near);
+    const double bar = (-far * near) / (far-near);
+    
+    Mat4d p;
+    p << S, 0.0, 0.0, 0.0,
+        0.0, S, 0.0, 0.0,
+        0.0, 0.0, foo, -1.0,
+        0.0, 0.0, bar, 0.0;
+    
+    return p;
+}
